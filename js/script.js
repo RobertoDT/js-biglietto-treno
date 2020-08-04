3a405f753a5a13b8ab2e537141d74ffce09767e2
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-08-04 15:15:27 +0200

    prima versione

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//chiedere utente numero km che vuole percorrere
+var distanza = parseInt(prompt("Inserisci la distanza in km!"));
+//chiedere utente età
+var eta = parseInt(prompt("inserisci la tua età"));
+
+
+//prezzo biglietto è 0.21euro/km
+//sconto 20% per minorenni
+//sconto 40% over 65
+var prezzo = distanza * 0.21;
+
+var scontoMinorenni = (prezzo * 20) / 100;
+var prezzoMinorenni = prezzo - scontoMinorenni;
+
+var scontoOver = (prezzo * 40) / 100;
+var prezzoOver = prezzo - scontoOver;
+
+if(eta < 18){
+  console.log(prezzoMinorenni);
+} else if (eta >= 65){
+  console.log(prezzoOver);
+} else{
+  console.log(prezzo);
+}
